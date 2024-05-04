910cf7fdb2bc1da44f1fc4c21af1555c35b8ed64
Agelio4 <danielzfunyt@gmail.com>
2024-05-04 18:16:53 -0500

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -86,7 +86,6 @@ export default {
                         <p class="error" v-for="error of errors">{{ error }}</p>
                     </div>
                     <div class="og">
-                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
                     </div>
                     <template v-if="editors">
                         <h3>List Editors</h3>
@@ -103,22 +102,16 @@ export default {
                         Completed without using hacks (however, FPS unlocker is allowed, up to 90fps)
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Have full raw footage for all completions.
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        The recording must also show the player hit the winpad, or the completion will be invalidated.
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        Do not use major skips.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
-                    </p>
-                    <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
-                    </p>
-                    <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Once a tower falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said tower.
                     </p>
                 </div>
             </div>
