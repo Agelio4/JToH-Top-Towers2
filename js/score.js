40e847a9bda58b771807fe59671dd9ffb8f6dfa8
Agelio4 <danielzfunyt@gmail.com>
2024-05-11 22:46:53 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-24.9975*Math.pow(rank-1, 0.645) + 500) *
+    let score = ((475 / 9801) * Math.pow((rank - 100), 2)) + 25
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
