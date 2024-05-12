d3a29665fb0d8b135516decb80b0fb39ef9dc6b7
Agelio4 <danielzfunyt@gmail.com>
2024-05-11 23:39:26 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,17 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = ((475 / 9801) * Math.pow((rank - 100), 2)) + 25 *
+   let score;
+// #1 - 10
+if(rank <= 10) {
+    score = (0.000002 * Math.pow(rank, 4)) - (0.00442 * Math.pow(rank, 3) + (0.7655 *  Math.pow(rank, 2) - (35.705703 * rank) + 534.944625;
+// #10 - 50
+} else if (rank <= 50) {
+    score = (0.000024 * Math.pow(rank, 4)) - (0.006123 * Math.pow(rank, 3)) + (0.566434 * Math.pow(rank, 2)) - (23.060752 * rank) + 429.851399;
+// #50 - 100 (wont give accurate score past 100)
+} else {
+    score = (-52.524/50) * (rank - 100) + 25;
+}
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
